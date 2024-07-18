434291fb0bdfa8e698e75a70bb7ac22a956c43bb
RainVina <ranjohnvina@gmail.com>
2024-07-18 15:19:27 +0800

    Added Hubspot chatbot

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Message sent! We will get back to you soon.');
-    // Here you can add code to handle form submission, e.g., send data to a server
 });

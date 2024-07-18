e186555dece5bd098869403fde7a5d9c5f949269
Ran <127465949+RainVina@users.noreply.github.com>
2024-07-18 15:17:27 +0800

    Add files via upload
    pushed files

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent! We will get back to you soon.');
+    // Here you can add code to handle form submission, e.g., send data to a server
+});
